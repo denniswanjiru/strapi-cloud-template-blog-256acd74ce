5360e126a216ba0a68e08525ba771fd7ac5c2e8f
Dennis Wanjiru <denniswanjiru71@gmail.com>
2025-05-21 17:29:52 +0300

    move production plugins configs to env

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,21 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  // "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:3000"], // or your frontend port
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      headers: "*",
+      credentials: true,
+    },
+  },
 ];
